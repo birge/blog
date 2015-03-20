780d8b646a40b07089226aeba1651d4bc894190b
Dexter Clark <dexter.clark@gmail.com>
2015-03-19 23:12:50 -0700

    add log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to blog
+    else
+      flash[:danger] = "This is for admins only"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
