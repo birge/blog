b459175a0e3071c1da294a7ceced27be0e03bf3c
Dexter Clark <dexter.clark@gmail.com>
2015-03-20 23:30:06 -0700

    add ability to add entries and tags to entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,4 +2,19 @@ class EntriesController < ApplicationController
   def new
     @entry = Entry.new
   end
+
+  def create
+    @entry = Entry.new(title: params[:entry][:title], body: params[:entry][:body])
+    if @entry.valid?
+      params[:entry][:spaced_tags].split(" ").each do |tag|
+        newTag = Tag.find_or_create_by(tagType: tag)
+        @entry.tags << newTag
+      end
+      @entry.save
+      redirect_to root_path
+    else
+      flash[:danger] = "Missing title or body"
+      render action: :new
+    end
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ActiveRecord::Base
   has_many :entry_tags
   has_many :tags, through: :entry_tags
+  attr_accessor :spaced_tags
 
   validates :title, presence: true
   validates :body, presence: true
