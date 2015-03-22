dd8cf09734c57c2be6259ab552a9d45b02364767
Dexter Clark <dexter.clark@gmail.com>
2015-03-21 17:21:49 -0700

    add ability to update entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -22,4 +22,22 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params[:id])
     @entry.spaced_tags = @entry.tags.map(&:tagType).join(" ")
   end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.title = params[:entry][:title]
+    @entry.body = params[:entry][:body]
+    if @entry.valid?
+      @entry.tags = []
+      params[:entry][:spaced_tags].split(" ").each do |tag|
+        newTag = Tag.find_or_create_by(tagType: tag)
+        @entry.tags << newTag
+      end
+      @entry.save
+      redirect_to root_path
+    else
+      flash[:danger] = "Missing title or body"
+      redirect_to edit_entry_path(@entry)
+    end
+  end
 end
