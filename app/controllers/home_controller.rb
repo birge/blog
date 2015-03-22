3c2317435d877d531a1ad7b0bcb800869bbb0769
Dexter Clark <dexter.clark@gmail.com>
2015-03-21 18:13:35 -0700

    add about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def home
     @entries = Entry.all
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get    'about'   => 'home#about'
   resources :entries
 end
