2320e067921ddce8f3d42eff164df1e92410a9f6
Dexter Clark <dexter.clark@gmail.com>
2015-03-22 18:03:35 -0700

    add pagination to main view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def home
-    @entries = Entry.all
+    @entries = Entry.paginate(page: params[:page], per_page: 5).order('created_at DESC')
   end
 
   def about
