296a53bfc3b2ab2685acf9f0af3f8e49e90d0405
Dexter Clark <dexter.clark@gmail.com>
2015-03-27 22:57:53 -0700

    Make tags clickable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def home
-    @entries = Entry.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    if params[:tag]
+      @entries = Tag.find_by(tagType: params[:tag]).entries.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    else
+      @entries = Entry.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    end
   end
 
   def about
