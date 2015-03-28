12448d5a01f38c2d7b885c0836336248a67571e5
Dexter Clark <dexter.clark@gmail.com>
2015-03-27 22:11:33 -0700

    add contact paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   root   'home#home'
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get    'about',  to: 'home#about'
+  get    'contact',to: 'messages#new'
+  post   'contact',to: 'messages#create'
+
   resources :entries
 
   get '*a',        to: 'home#wrong_url'
